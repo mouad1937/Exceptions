4726bf3d81bd3e9b4f3053d877d4ce52a15ad167
mouad <mouadhisoka1937@gmail.com>
2023-05-17 18:36:42 -0400

    issue fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,27 +8,27 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+while (true){
         try {
             System.out.println("Enter the numerator :");
-            int numerator= scanner.nextInt();
+            int numerator = scanner.nextInt();
             System.out.println("Enter the denominator :");
-            int denominator= scanner.nextInt();
+            int denominator = scanner.nextInt();
 
-            int result = numerator/denominator;
-            System.out.println("Result :" + result );
-        } catch (ArithmeticException a){
-            System.out.println("Error : Invalid method ' cannot divide by zero ");
-        }catch (InputMismatchException p){
-            System.out.println("Error : Invalid Input , please Enter whole Numbers only ! thank you 20");
-            scanner.nextInt();
+            int result = numerator / denominator;
+            System.out.println("Result :" + result);
+        } catch( ArithmeticException a){
+                System.out.println("Error : Invalid method ' cannot divide by zero ");
+            }catch(InputMismatchException p){
+                System.out.println("Error : Invalid Input , please Enter whole Numbers only ! thank you ");
+                scanner.nextLine();
+            }
+        }
+        }
         }
 
 
 
 
-    }
 
 
-
-}
