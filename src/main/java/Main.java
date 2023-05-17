52b62c9e8fba8b9f259c7592963e87a312efa907
mouad <mouadhisoka1937@gmail.com>
2023-05-17 13:52:00 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import org.w3c.dom.ls.LSOutput;
+
+import javax.crypto.spec.PSource;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter the numerator :");
+            int numerator= scanner.nextInt();
+            System.out.println("Enter the denominator :");
+            int denominator= scanner.nextInt();
+
+            int result = numerator/denominator;
+            System.out.println("Result :" + result );
+        } catch (ArithmeticException a){
+            System.out.println("Error : Invalid method ' cannot divide by zero ");
+        }catch (InputMismatchException p){
+            System.out.println("Error : Invalid Input , please Enter whole Numbers only ! thank you 20");
+            scanner.nextInt();
+        }
+
+
+
+
+    }
+
+
+
+}
